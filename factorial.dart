cce5de3bece05adff6b828f4ca66bd728dd5db5e
Wilwonka06 <heieihei183@gmail.com>
2025-05-08 19:44:57 -0500

    Factorial

---
+++ factorial.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+void main() {
+  print('Ingresa el número para realizar el factorial:');
+  int fact = int.parse(stdin.readLineSync()!);
+
+  // Método recursivo
+  int factorialRecursivo(int n) {
+    if (n <= 1) {
+      return 1;
+    }
+    return n * factorialRecursivo(n - 1);
+  }
+
+  // Método iterativo
+  int factorialIterativo(int n) {
+    int resultado = 1;
+    for (int i = 1; i <= n; i++) {
+      resultado *= i;
+    }
+    return resultado;
+  }
+
+  int calculoRecursivo = factorialRecursivo(fact);
+  int calculoIterativo = factorialIterativo(fact);
+  
+  print('El factorial de $fact es:');
+  print('Usando recursividad: $calculoRecursivo');
+  print('Usando iteración: $calculoIterativo');
+}
